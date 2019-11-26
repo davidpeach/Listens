c4b279bac9a08dc10756b2eb5d1575c5dfcf1a36
David Peach <mail@davidpeach.co.uk>
2019-11-26 21:23:08 +0000

    Continue routing fixes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,30 +2,25 @@ import VueRouter from 'vue-router';
 
 let routes = [
     {
-        path: '/listens',
-        redirect: { name: 'listen' }
-    },
-    {
         name: 'album',
-        path: '/listens/albums/',
+        path: '/albums/',
         component: require('./views/Albums.vue').default,
         pathToRegexpOptions: { strict: true }
     },
     {
         name: 'artist',
-        path: '/listens/artists/:page*/',
+        path: '/artists/:page*/',
         component: require('./views/Artists.vue').default,
         pathToRegexpOptions: { strict: true }
     },
     {
         name: 'listen',
-        path: '/listens/',
+        path: '/',
         component: require('./views/Listens.vue').default,
         pathToRegexpOptions: { strict: true }
     }
 ];
 
 export default new VueRouter({
-    //mode: 'history',
     routes
 });
